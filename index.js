21a1c1f7da084fb2bb9c0373bfccb45c56ba871d
Oskar Kaminski <oskar.kaminski@smtsoftware.com>
2016-08-07 23:29:18 +0200

    Added logic to create random winning field + styles

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 angular.module('app',[]).controller('app', function($scope){
-    $scope.test = 'Oliver'
+    $scope.test = 'Oliver';
+    $scope.checkResult = function(){
+        $scope.showResult = true;
+    };
+    $scope.scratch = [];
+    var winningIndex = Math.floor(Math.random()*4);
+    for(var i = 0; i<4; i++){
+        $scope.scratch[i] = i === winningIndex ? 'won' : 'lost';
+    }
 });
